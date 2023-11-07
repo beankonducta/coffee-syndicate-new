ed8f5070a1d64c6c007ce181e0dafdc421e3362b
Patrick Andrews <beankonducta@Patricks-MBP.hsd1.ut.comcast.net>
2023-11-07 09:24:47 -0700

    add transforms

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,20 @@
 <template>
   <div id="app">
-    <h1 id="h-1" :style="{ top: h1Top }">
-      Let's get that machine working again.
-    </h1>
-    <h1 id="y"> {{  position[1] }}</h1>
-    <div v-if="position[1] <= 8500" id="bottom-curtain"></div>
-    <div v-if="position[1] >= 8500" id="center-curtain"></div>
-    <img
-      id="header"
-      src="./assets/Coffee Syndicate Logo Full.svg"
-      alt="Coffee Syndicate Logo"
-    />
-    <img
-      id="pf"
-      src="./assets/icon-01.png"
-      :style="{ top: pfTop, right: pfRight }"
-      alt="Portafilter"
-    />
-    <img
-      id="basket"
-      src="./assets/icon-03.png"
-      :style="{ top: basketTop, right: basketRight }"
-      alt="Basket" 
-    />
-    <img
-      id="coffee"
-      src="./assets/icon-07.png"
-      :style="{ top: coffeeTop, right: coffeeRight }"
-      alt="Coffee"
-    />
-    <img
-      id="puck"
-      src="./assets/icon-02.png"
-      :style="{ top: puckTop, right: puckRight, opacity: puckOpacity }"
-      alt="Puck"
-    />
-    <img
-      id="tamper"
-      src="./assets/icon-05.png"
-      :style="{ top: tamperTop, right: tamperRight }"
-      alt="Tamper"
-    />
-    <img
-      id="screw"
-      src="./assets/icon-04.png"
-      :style="{ top: screwTop, right: screwRight }"
-      alt="Screw"
-    />
-    <img
-      id="screen"
-      src="./assets/icon-06.png"
-      :style="{ top: screenTop, right: screenRight }"
-      alt="Screen"
-    />
-    <img
-      id="tube"
-      src="./assets/icon-08.png"
-      :style="{ top: tubeTop, right: tubeRight }"
-      alt="Tube"
-    />
-    <img
-      id="drip"
-      src="./assets/icon-09.png"
-      :style="{ top: dripTop, right: dripRight }"
-      alt="Drip"
-    />
-    <img
-      id="spro"
-      v-if="position[1] >= 8500"
-      src="./assets/icon-10.png"
-      :style="{ top: sproTop, right: sproRight }"
-      alt="Spro"
-    />
+    <!-- TODO: MAKE TRANSLATIONS ONE STEP RATHER THAN EACH SCROLL TICK -->
+    <h1 id="y"> {{ position[1] }}</h1>
+    <h1 id="water-x"> {{ bottomWaterRight }}</h1>
+    <div id="curtain" :style="{ top: curtainTop }"></div>
+    <div id="white" :style="{ top: whiteTop }"></div>
+    <img id="header" src="./assets/CS_Logo.png" alt="Coffee Syndicate Logo" />
+    <img id="knob" src="./assets/knob.png" alt="knob" :style="{ transform: translateString(0, knobTop, knobRotation) }" />
+    <img id="spout" src="./assets/spout.png" alt="spout" :style="{ transform: translateString(0, spoutTop), msTransform: translateString(0, spoutTop), webkitTransform: translateString(0, spoutTop) }" />
+    <img id="water" src="./assets/water.png" alt="water" :style="{ transform: translateString(0, waterTop), msTransform: translateString(0, waterTop), webkitTransform: translateString(0, waterTop) }" />
+    <img id="bottom-water" src="./assets/bottom_water.svg" alt="bottom-water"
+      :style="{ transform: translateString(bottomWaterRight, bottomWaterTop), msTransform: translateString(bottomWaterRight, bottomWaterTop), webkitTransform: translateString(bottomWaterRight, bottomWaterTop) }" />
+    <div id="content" :class="position[1] >= 13500 ? 'fadein' : 'fadeout'" :style="{ top: contentTop }">
+      <h1 id="about-header">COFFEE SYNDICATE OFFERS WORLD CLASS COFFEE EQUIPMENT SERVICE, SALES & CUSTOM SOFTWARE
+        SOLUTIONS</h1>
+    </div>
   </div>
 </template>
 
@@ -82,110 +25,110 @@ export default {
   mixins: [windowScrollPosition("position")],
   name: "App",
   components: {},
-  methods: {},
+  data() {
+    return {
+      curtainTop: 0+ "px",
+      spoutTop: 200+ "px",
+      knobTop: 20+ "px",
+      waterTop: window.innerHeight * 1.5+ "px",
+      bottomWaterTop: window.innerHeight * 1.5 + 900+ "px",
+      bottomWaterRight: -200 + "%",
+      knobRotation: 0+"deg",
+      whiteTop: -100000+"px",
+      contentTop: 300+"px",
+      contentRight: 0+"px"
+    };
+  },
+  methods: {
+    translateString(x = '0px', y ='0px', angle = '0deg') {
+      return `translate(${x}, ${y}) rotate(${angle})`
+    }
+  },
   computed: {
     scrollAmount() {
       return window.scrollY;
     },
-    h1Top() {
-      return window.innerHeight / 2 - this.position[1] + "px";
+    _curtainTop() {
+      if (this.position[1] > 5000)
+        return (-this.position[1] + 5000) / 8 + "px";
+      else
+        return 0 + "px";
     },
-    pfTop() {
-      return window.innerHeight / 4 + "px";
+    _spoutTop() {
+      if (this.position[1] > 5000)
+        return (-this.position[1] + 5000) / 8 + 200 + "px";
+      else
+        return 200 + "px";
     },
-    pfRight() {
-      let pos = -1000 + this.position[1];
-      if (pos <= window.innerWidth / 2 - 500) {
-        return pos + "px";
-      } else {
-        return window.innerWidth / 2 - 500 + "px";
-      }
+    _knobTop() {
+      if (this.position[1] > 5000)
+        return (-this.position[1] + 5000) / 8 + 20 + "px"; 
+      else
+        return 20 + "px";
     },
-    basketTop() {
-      let pos = -1500 + this.position[1];
-      if (pos <= +this.pfTop.replace("px", "")) {
-        return pos + "px";
-      } else {
-        return this.pfTop;
-      }
+    _waterTop() {
+      if (this.position[1] > 5000)
+        return (this.position[1]) / 28 - 900 + "px";
+      else
+        return window.innerHeight * 1.5 + "px";
     },
-    basketRight() {
-      return window.innerWidth / 2 - 500 + "px";
+    _bottomWaterTop() {
+      if (this.position[1] > 7000)
+        return (-this.position[1] + 7000) / 6 + (window.innerHeight * 1.5) + "px";
+      else
+        return (window.innerHeight * 1.5) + 900 + "px";
     },
-    coffeeTop() {
-      let pos = -2800 + this.position[1];
-      return pos + "px";
+   _bottomWaterRight() {
+      if (this.position[1] > 10000)
+        return (this.position[1] - 15000) / 8 + "px";
+      else
+        return -200 + "%";
     },
-    coffeeRight() {
-      return window.innerWidth / 2 - 500 + "px";
+    _knobRotation() {
+      return this.position[1] / 80 + "deg";
     },
-    puckTop() {
-      let offset = this.position[1] >= 2600 ? (2600 - this.position[1]) / 15 : 0;
-      if (offset < -40) {
-        offset = -40;
-      }
-      let pos = +this.basketTop.replace("px", "") +50 + offset;
-      return pos + "px";
+    _whiteTop() {
+      return (15000 - this.position[1]) / 6 + "px";
     },
-    puckRight() {
-      return window.innerWidth / 2 - 500 + "px";
+    _contentTop() {
+      return (15000 - this.position[1]) / 6 + 300 + "px";
     },
-    puckOpacity() {
-      return this.position[1] >= 2600 ? (this.position[1] - 2600) / 140 : 0;
-    },
-    tamperTop() {
-      let pos = -3700 + this.position[1];
-      if(this.position[1] >= 3900)
-       pos = 200;
-      if(this.position[1] >= 5000)
-        pos = 200 + (5000 - this.position[1])
-      return pos + "px";
-    },
-    tamperRight() {
-      return window.innerWidth / 2 - 500 + "px";
-    },
-    screwTop() {
-      let pos = -6300 + this.position[1];
-      if(this.position[1] >= 6500)
-        pos = 200;
-      return pos + "px";
-    },
-    screwRight() {
-      return window.innerWidth / 2 - 500 + "px";
-    },
-    screenTop() {
-      let pos = -6450 + this.position[1];
-      if(this.position[1] >= 6650)
-        pos = 200;
-      return pos + "px";
-    },
-    screenRight() {
-      return window.innerWidth / 2 - 500 + "px";
-    },
-    tubeTop() {
-      let pos = -6600 + this.position[1];
-      if(this.position[1] >= 6750)
-        pos = 150;
-      return pos + "px";
-    },
-    tubeRight() {
-      return window.innerWidth / 2 - 500 + "px";
-    },
-    dripTop() {
-      let pos = -7600 + this.position[1];
-      return pos + "px";
-    },
-    dripRight() {
-      return window.innerWidth / 2 - 500 + "px";
-    },
-    sproTop() {
-      let pos = (this.position[1] -10000);
-      return pos + "px";
-    },
-    sproRight() {
-      return window.innerWidth / 2 - 500 + "px";
+    _contentRight() {
+      return (15000 - this.position[1]) / 7 + "px";
     },
   },
+  watch: {
+    _curtainTop(val) {
+      this.curtainTop = val;
+    },
+    _spoutTop(val) {
+      this.spoutTop = val;
+    },
+    _knobTop(val) {
+      this.knobTop = val;
+    },
+    _waterTop(val) {
+      this.waterTop = val;
+    },
+    _bottomWaterTop(val) {
+      this.bottomWaterTop = val;
+    },
+    _bottomWaterRight(val) {
+      this.bottomWaterRight = val;
+    },
+    _knobRotation(val) {
+      this.knobRotation = val;
+    },
+    _whiteTop(val) {
+      this.whiteTop = val;
+    },
+    _contentTop(val) {
+      this.contentTop = val;
+    },
+    _contentRight(val) {
+      this.contentRight = val;
+    },
+  }
 };
 </script>
 
@@ -195,31 +138,39 @@ body {
   padding: 0;
   font-family: sans-serif;
   overscroll-behavior: none;
+  background: #002b49;
 }
 
 #app {
   display: flex;
   flex-direction: column;
-  height: 15000px;
-  background: #002b49;
+  height: 15500px;
+  background: linear-gradient(180deg, #002b49 0%, #002b49 50%, #002947 100%);
 }
 
-#bottom-curtain {
+#curtain {
   position: fixed;
-  height: 40%;
   width: 100%;
+  height: 820px;
   background: #002b49;
-  top: 60%;
   z-index: 2;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
 }
 
-#center-curtain {
-  position: fixed;
-  height: 66%;
+#white {
+  background: linear-gradient(180deg, #ffffff 0%, #ffffff 50%, #fafafa 100%);
   width: 100%;
-  background: #002b49;
-  top: 0%;
-  z-index: 2;
+  height: 1500px;
+  position: fixed;
+  left: 0;
+  z-index: 5;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
 }
 
 #y {
@@ -229,78 +180,108 @@ body {
   color: white;
   font-size: 20px;
   padding: 10px;
+  z-index: 10;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
 }
 
-#h-1 {
+#water-x {
   position: fixed;
-  width: 100%;
-  text-align: center;
+  top: 30px;
+  right: 0;
+  color: white;
+  font-size: 20px;
+  padding: 10px;
+  z-index: 10;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
 }
 
 #header {
   position: fixed;
-  height: 200px;
+  width: 250px;
   top: 15px;
+  left: calc(50% - 125px);
+  z-index: 10;
+  filter: invert(0);
+  mix-blend-mode: difference;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
+}
+
+#spout {
+  position: fixed;
+  width: 300px;
+  left: 56px;
+  z-index: 3;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
+}
+
+#knob {
+  position: fixed;
+  width: 225px;
+  left: 0;
+  z-index: 4;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
+}
+
+#water {
+  position: fixed;
+  width: 100px;
+  left: 250px;
+  z-index: 1;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
+}
+
+#bottom-water {
+  position: fixed;
+  width: 300%;
+  z-index: 1;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
+}
+
+#about-header {
+  position: fixed;
+  width: 80%;
+  left: 10%;
+  margin-top: 300px;
+  color: #002b49;
+  padding: 10px;
+  text-align: center;
+  transition: top 0s linear;
+  -moz-transition: -moz-transform 0s linear;
+  -ms-transition: -ms-transform 0s linear;
+  -webkit-transition: -webkit-transform 0s linear;
+}
+
+#content {
+  width: 100%;
+  transition: opacity .5s ease-in-out;
   z-index: 10;
 }
 
-#pf {
-  position: fixed;
-  width: 600px;
-  z-index: 3;
+.fadeout {
+  opacity: 0;
 }
 
-#basket {
-  position: fixed;
-  width: 600px;
-  z-index: 3;
-}
-
-#coffee {
-  position: fixed;
-  width: 600px;
-  z-index: 1;
-}
-
-#puck {
-  position: fixed;
-  width: 600px;
-  z-index: 3;
-}
-
-#tamper {
-  position: fixed;
-  width: 600px;
-  z-index: 3;
-}
-
-#screw {
-  position: fixed;
-  width: 600px;
-  z-index: 3;
-}
-
-#screen {
-  position: fixed;
-  width: 600px;
-  z-index: 3;
-}
-
-#tube {
-  position: fixed;
-  width: 600px;
-  z-index: 3;
-}
-
-#drip {
-  position: fixed;
-  width: 600px;
-  z-index: 1;
-}
-
-#spro {
-  position: fixed;
-  width: 600px;
-  z-index: 1;
-}
-</style>
+.fadein {
+  opacity: 1;
+}</style>
